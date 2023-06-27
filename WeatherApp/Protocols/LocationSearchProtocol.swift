5e482702e2247841afa918ed669a581e054cb676
Daniel Person <daniel.person@yahoo.com>
2023-06-27 13:38:38 -0400

    Update LocationSearchProtocol.swift

--- WeatherApp/Protocols/LocationSearchProtocol.swift
+++ WeatherApp/Protocols/LocationSearchProtocol.swift
@@ -11,7 +11,7 @@ import Foundation
 protocol LocationSearchProtocol {
     /// Protocol to search for a location from a text input
     /// - Parameter location: The text input to try to match a location
-    /// - Returns: An array of `LocationResponse` objects as defined by the Open Weather service
+    /// - Returns: An array of `Location` objects as defined by the Open Weather service
     func searchForLocation(_ location: String) async throws -> [Location]
     
     /// Start a process to get the current location of the user
